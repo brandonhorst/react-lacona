e9eb16118f722e30df16d319e489917b3f436500
Brandon Horst <brandonhorst@gmail.com>
2015-03-19 14:45:51 -0400

    Simplified data structure

--- demo/script.js
+++ demo/script.js
@@ -2,25 +2,19 @@ var React = require('react')
 var reactLacona = React.createFactory(require('..'))
 
 var outputs = [{
-  id: 0,
-  data: {
-    match: [{string: 'match', category: 'match', input: true}],
-    suggestion: [
-      {string: 'sugg', category: 'suggestion', input: false},
-      {string: 'estion', category: 'suggestion', input: true}
-    ],
-    completion: [{string: 'completion', category: 'completion', input: false}]
-  }
+  match: [{string: 'match', category: 'match', input: true}],
+  suggestion: [
+    {string: 'sugg', category: 'suggestion', input: false},
+    {string: 'estion', category: 'suggestion', input: true}
+  ],
+  completion: [{string: 'completion', category: 'completion', input: false}]
 }, {
-  id: 1,
-  data: {
-    match: [{string: 'match2', category: 'match', input: true}],
-    suggestion: [
-      {string: 'sugg', category: 'suggestion', input: false},
-      {string: 'estion2', category: 'suggestion', input: true}
-    ],
-    completion: [{string: 'completion2', category: 'completion', input: false}]
-  }
+  match: [{string: 'match2', category: 'match', input: true}],
+  suggestion: [
+    {string: 'sugg', category: 'suggestion', input: false},
+    {string: 'estion2', category: 'suggestion', input: true}
+  ],
+  completion: [{string: 'completion2', category: 'completion', input: false}]
 }]
 
 var demo = React.createFactory(React.createClass({
